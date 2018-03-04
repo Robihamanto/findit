7b03e2e3341358a124b8093c57ea34f8733a2688
Robihamanto <rrobie44@gmail.com>
2018-03-04 20:05:08 +0700

    First init 😃

---
+++ Findit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Findit
+//
+//  Created by Robihamanto on 04/03/18.
+//  Copyright © 2018 Robihamanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
